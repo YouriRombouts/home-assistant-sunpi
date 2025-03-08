624f29dec7728d41d5fe4b6c2b502586c19aa703
YouriRombouts <youri.rombouts@gmail.com>
2025-03-08 17:53:08 +0100

    Redo config

--- custom_components/quatt/config_flow.py
+++ custom_components/quatt/config_flow.py
@@ -1,104 +1,85 @@
-"""Adds config flow for SunPi."""
+"""Config flow for the SunPi integration."""
 
 from __future__ import annotations
 
-import ipaddress
+import logging
+from typing import Any
 
 import voluptuous as vol
 
-from homeassistant import config_entries, data_entry_flow
-from homeassistant.components import dhcp
-from homeassistant.components.sensor import SensorDeviceClass
 from homeassistant.config_entries import (
     ConfigFlow,
+    ConfigFlowResult,
 )
-from homeassistant.const import CONF_IP_ADDRESS, CONF_SCAN_INTERVAL
-from homeassistant.core import callback
-from homeassistant.helpers import selector
-from homeassistant.helpers.aiohttp_client import async_create_clientsession
+from homeassistant.const import (
+    CONF_HOST,
+)
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.exceptions import HomeAssistantError
 
-from .api import (
-    SunPiApiClient,
-    APIConnectionError,
-    APITimeoutError,
+from .api import SunPiApiClient, APIConnectionError, APITimeoutError
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+# TODO adjust the data schema to the data that you need
+STEP_USER_DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_HOST, description={"suggested_value": "10.10.10.1"}): str,
+    }
 )
 
-from .const import (
-    DOMAIN,
-    LOGGER,
-)
+
+async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
+    """Validate the user input allows us to connect."""
+
+    api = SunPiApiClient(data[CONF_HOST])
+    try:
+        await hass.async_add_executor_job(api.async_get_data)
+        # If you cannot connect, raise CannotConnect
+        # If the authentication is wrong, raise InvalidAuth
+    except APITimeoutError as err:
+        raise TimeoutError from err
+    except APIConnectionError as err:
+        raise CannotConnect from err
+    return {"title": f"Example Integration - {data[CONF_HOST]}"}
 
 
-class SunPiFlowHandler(ConfigFlow, domain=DOMAIN):
-    """Config flow for SunPi."""
+class SunPiConfigFlow(ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for the SunPi Integration."""
 
     VERSION = 1
-
-    def __init__(self) -> None:
-        """Initialize a SunPi flow."""
-        self.ip_address: str | None = None
-        self.hostname: str | None = None
-
-    async def _test_credentials(self, ip_address: str) -> str:
-        """Validate credentials."""
-        client = SunPiApiClient(ip_address)
-        return await client.async_get_data()
+    _input_data: dict[str, Any]
 
     async def async_step_user(
-        self,
-        user_input: dict | None = None,
-    ) -> config_entries.FlowResult:
-        """Handle a flow initialized by the user."""
-        _errors = {}
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Handle the initial step."""
+        errors: dict[str, str] = {}
+
         if user_input is not None:
             try:
-                data = await self._test_credentials(
-                    ip_address=user_input[CONF_IP_ADDRESS],
-                )
-            except APIConnectionError as exception:
-                LOGGER.warning(exception)
-                _errors["base"] = "unknown"
-            except APITimeoutError as exception:
-                LOGGER.error(exception)
-                _errors["base"] = "timeout"
-            else:
-                if data is not None:
-                    # Check if this cic has already been configured
-                    await self.async_set_unique_id("SunPi")
-                    self._abort_if_unique_id_configured()
+                info = await validate_input(self.hass, user_input)
+            except CannotConnect:
+                errors["base"] = "cannot_connect"
+            except TimeoutError:
+                errors["base"] = "timeout"
+            except Exception:  # pylint: disable=broad-except
+                _LOGGER.exception("Unexpected exception")
+                errors["base"] = "unknown"
 
-                    return self.async_create_entry(
-                        title="SunPi",
-                        data=user_input,
-                    )
+            if "base" not in errors:
+                # Validation was successful, so create a unique id for this instance of your integration
+                # and create the config entry.
+                await self.async_set_unique_id(info.get("title"))
+                self._abort_if_unique_id_configured()
+                return self.async_create_entry(title=info["title"], data=user_input)
 
-
+        # Show initial form.
         return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(
-                        CONF_IP_ADDRESS,
-                        default=(user_input or {}).get(CONF_IP_ADDRESS),
-                    ): selector.TextSelector(
-                        selector.TextSelectorConfig(
-                            type=selector.TextSelectorType.TEXT
-                        ),
-                    ),
-                }
-            ),
-            errors=_errors,
+            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
         )
 
-    async def async_step_confirm(
-        self, user_input=None
-    ) -> data_entry_flow.ConfigFlowResult:
-        """Allow the user to confirm adding the device."""
-        if user_input is not None:
-            # Use the hostname instead of the ip
-            return self.async_create_entry(
-                title="SunPi",
-                data={CONF_IP_ADDRESS: self.ip_address}
-            )
 
-        return self.async_show_form(step_id="confirm")
+class CannotConnect(HomeAssistantError):
+    """Error to indicate we cannot connect."""
