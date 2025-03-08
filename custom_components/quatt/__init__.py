3a803d1e9473ef5d7d2537829b5b8a723893e8ed
YouriRombouts <youri.rombouts@gmail.com>
2025-03-08 18:15:39 +0100

    Use HOST

--- custom_components/quatt/__init__.py
+++ custom_components/quatt/__init__.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_IP_ADDRESS, Platform
+from homeassistant.const import CONF_HOST, Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
 
@@ -26,7 +26,7 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry[Runti
     coordinator = SunPiDataUpdateCoordinator(
         hass=hass,
         update_interval=config_entry.options.get(SCAN_INTERVAL),
-        client=SunPiApiClient(config_entry.data[CONF_IP_ADDRESS]),
+        client=SunPiApiClient(config_entry.data[CONF_HOST]),
     )
     await coordinator.async_config_entry_first_refresh()
 
@@ -56,9 +56,3 @@ async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry[Runt
 
     # Unload platforms and return result
     return await hass.config_entries.async_unload_platforms(config_entry, PLATFORMS)
-
-async def _get_data_for_test(hass: HomeAssistant, ip_address: str) -> bool:
-    """Validate credentials."""
-    client = SunPiApiClient(ip_address=ip_address)
-    data = await client.async_get_data()
-    return data is not None
